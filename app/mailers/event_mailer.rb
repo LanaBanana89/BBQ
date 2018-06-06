30e401e980e1303e9a06fc67722c8086a49fbf44
LanaBanana89 <s.glazyrina@mail.ru>
2018-06-06 20:43:39 +0300

    add event_mailer

---
+++ app/mailers/event_mailer.rb
@@ -0,0 +1,27 @@
+class EventMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.event_mailer.subscription.subject
+  #
+  def subscription(event, subscription)
+    @email = subscription.user_email
+    @name = subscription.user_name
+    @event = event
+
+    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.event_mailer.comment.subject
+  #
+  def comment(event, comment, email)
+    @comment = comment
+    @event = event
+
+    mail to: email, subject: "Новый комментарий @ #{event.title}"
+  end
+end
