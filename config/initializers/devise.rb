b4b0409698749e8b478584b3e6336fbd82d6a49d
LanaBanana89 <s.glazyrina@mail.ru>
2018-06-06 21:01:20 +0300

    add default email address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'svetlana.glazyrina89@gmail.com'
+  config.mailer_sender = 'lets-go-bbq@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
