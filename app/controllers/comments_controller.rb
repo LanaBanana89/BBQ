54bef3d9e53792f9743f24425caa95c6bfd3bd69
LanaBanana89 <s.glazyrina@mail.ru>
2018-06-08 19:46:42 +0300

    correct comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -44,7 +44,11 @@ class CommentsController < ApplicationController
 
     def notify_subscribers(event, comment)
     # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [comment.user.email]).uniq
+    if comment.user.present?
+      all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [comment.user.email]).uniq
+    else
+      all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    end
 
     # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
     # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
