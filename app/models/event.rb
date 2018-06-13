0195e15926b43d016bc81e5e61276546d6fe093c
LanaBanana89 <s.glazyrina@mail.ru>
2018-06-13 12:19:48 +0300

    add pincode to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -22,4 +22,8 @@ class Event < ActiveRecord::Base
   def visitors
     (subscribers + [user]).uniq
   end
+
+  def pincode_valid?(pin2check)
+    pincode == pin2check
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517175632) do
+ActiveRecord::Schema.define(version: 20180609165053) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180517175632) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
